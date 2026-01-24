140f744cb5db1faad4fb115a8c287e9daf906a41
Rohit-Dhade <rohitdhade99@gmail.com>
2026-01-24 19:43:15 +0530

    UI of selecting date-based features done.

--- React_UI/react_ui/src/Components/MainPart.jsx
+++ React_UI/react_ui/src/Components/MainPart.jsx
@@ -29,9 +29,9 @@ const MainPart = () => {
               <option value="" disabled selected>
                 Select a date column
               </option>
-              <option value="created_at">Option 1</option>
-              <option value="order_date">Option 2</option>
-              <option value="signup_time">Option 3</option>
+              {["Date","Age","Weight","Height","bmi","college","year of interest"].map((item)=>(
+                <option value="created_at">{item}</option>
+              ))}
             </select>
             <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
               ▼
@@ -47,12 +47,38 @@ const MainPart = () => {
       <div className="h-[70%] w-[0.15%] bg-gray-300 mt-5"></div>
 
       {/* right part */}
-      <div className="h-[70%] w-1/2 mt-5 bg-white">
-        <div className="flex items-center gap-2">
+      <div className="w-1/2 bg-white p-5 rounded-xl shadow-sm">
+        <div className="flex items-center gap-2 mb-4">
           <span className="text-blue-600 text-2xl font-bold">3.</span>
           <h2 className="text-lg font-semibold text-gray-900">
             Select Feature to Generate
           </h2>
+        </div>
+
+        <div className="flex w-full gap-6">
+          <div className="w-1/2 rounded-xl border border-blue-300 overflow-hidden">
+            <div className="bg-[#EFF3FB] w-full text-center py-2 text-sm font-semibold text-gray-700">
+              Basic Features
+            </div>
+            {["Week", "Day", "Day of week", "is Weekend"].map((item) => (
+              <div className="flex items-center gap-3 px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 cursor-pointer">
+                <input type="checkbox" className="accent-blue-600" />
+                {item}
+              </div>
+            ))}
+          </div>
+
+          <div className="w-1/2 rounded-xl border border-blue-300 overflow-hidden">
+            <div className="bg-[#EFF3FB] w-full text-center py-2 text-sm font-semibold text-gray-700">
+              Advance Features
+            </div>
+            {["Week Number","Quarter","Days since start","Days since previous"].map((item) => (
+              <div className="flex items-center gap-3 px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 cursor-pointer">
+                <input type="checkbox" className="accent-blue-600" />
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
